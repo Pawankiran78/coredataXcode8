95b337ae50b22210135955174dbb512f48e9628f
Sudhakar Parsi <ssss@Sudhakars-Mac-mini.local>
2017-07-23 12:15:51 -0700

    Initial Commit

---
+++ coredataXcode8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredataXcode8
+//
+//  Created by Sudhakar Parsi on 7/23/17.
+//  Copyright © 2017 Pawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredataXcode8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredataXcode8
+//
+//  Created by Sudhakar Parsi on 7/23/17.
+//  Copyright © 2017 Pawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
